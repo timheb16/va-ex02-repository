30e109d927be28426fb0f49192da26561efa88e4
timheb16 <65227120+timheb16@users.noreply.github.com>
2022-05-23 16:02:17 +0200

    Add files via upload

---
+++ kmeans.js
@@ -0,0 +1,90 @@
+/* k-means implementation in 2D */
+
+/**
+ * Calculates the mean for x and y of the given data points.
+ *
+ * @param {[{ x, y, centroid_index }, ...]} datapoints - given data points to calculate measure on, whereas the array contains the data points; centroid_index is not needed here, but is part of the default data structure
+ * @returns {{x, y}} - the measure (here: mean)
+ */
+function mean(datapoints) {
+  // TODO
+  return { x: 0, y: 0 }
+}
+/**
+ * Calculates the median for x and y of the given data points.
+ *
+ * @param {[{ x, y, centroid_index }, ...]} datapoints - given data points to calculate measure on, whereas the array contains the data points; centroid_index is not needed here, but is part of the default data structure
+ * @returns {{x, y}} - the measure (here: median)
+ */
+function median(datapoints) {
+  // TODO
+  return { x: 0, y: 0 }
+}
+
+/**
+ * Calculates the euclidian distance between two points in space.
+ *
+ * @param {{ x, y, centroid_index }} point1 - first point in space
+ * @param {{ x, y, centroid_index }} point2 - second point in space
+ * @returns {Number} - the distance of point1 and point2
+ */
+function euclid(point1, point2) {
+  // TODO
+  return 0
+}
+
+/**
+ * Calculates the manhattan distance between two points in space.
+ *
+ * @param {{ x, y, centroid_index }} point1 - first point in space
+ * @param {{ x, y, centroid_index }} point2 - second point in space
+ * @returns {Number} - the distance of point1 and point2
+ */
+function manhattan(point1, point2) {
+  // TODO
+  return 0
+}
+
+/**
+ * Assigns each data point according to the given distance function to the nearest centroid.
+ *
+ * @param {[{ x, y, centroid_index }, ...]} datapoints - all available data points
+ * @param {[{ x, y }, ... ]} centroids - current centroids
+ * @param {Function} distance_function - calculates the distance between positions
+ * @returns {[{ x, y, centroid_index }, ...]} - data points with new centroid-assignments
+ */
+function assign_datapoints_to_centroids(
+  datapoints,
+  centroids,
+  distance_function
+) {
+  // TODO
+  return datapoints
+}
+
+/**
+ * Calculates for each centroid it's new position according to the given measure.
+ *
+ * @param {[{ x, y, centroid_index }, ...]} datapoints - all available data points
+ * @param {[{ x, y }, ... ]} centroids - current centroids
+ * @param {Function} measure_function - measure of data set (e.g. mean-function, median-function, ...)
+ * @returns {{[{ x, y }, ... ], Boolean}} - centroids with new positions, and true of at least one centroid position changed
+ */
+function calculate_new_centroids(datapoints, centroids, measure_function) {
+  let centroids_changed = false
+  // TODO
+  return { centroids, centroids_changed }
+}
+
+/**
+ * Generates random centroids according to the data point boundaries and the specified k.
+ *
+ * @param {[{ x, y }, ...]} datapoints - all available data points
+ * @param {Number} k - number of centroids to be generated as a Number
+ * @returns {[{ x, y }, ...]} - generated centroids
+ */
+function get_random_centroids(datapoints, k) {
+  let centroids = []
+  // TODO
+  return centroids
+}
